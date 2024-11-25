542ba6de8537efc90725e916291a25d2f6869630
Luke Vergone <178243199+LukeVergone@users.noreply.github.com>
2024-11-25 23:11:20 +0800

    feat: Add initializer methods to Display and Sensor classes.

--- src/display.py
+++ src/display.py
@@ -1,2 +1,11 @@
 class Display:
-    pass
+    def __init__(self, id, message = "", is_on = False, car_park=None):
+        self.id = id
+        self.message = message
+        self.is_on = is_on
+        self.car_park = car_park
+
+    def __str__(self):
+        print(f"Display {self.id}: Welcome top the car park.")
+
+
--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,17 @@
 class Sensor:
-    pass
+    def __init__(self, id, is_active, car_park):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+    def __str__(self):
+        print(f"Sensor {self.id}. Status (is active?): {self.is_active}")
+
+
+class EntrySensor(Sensor):
+    ...
+
+class ExitSensor(Sensor):
+    ...
+
+
