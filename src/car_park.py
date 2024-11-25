863cce81a628ec95ef4228434edc0df01528c671
Luke Vergone <178243199+LukeVergone@users.noreply.github.com>
2024-11-25 22:59:57 +0800

    feat: add carpark initialize method

--- src/car_park.py
+++ src/car_park.py
@@ -1,3 +1,13 @@
 class CarPark:
-    pass
+
+    def __init__(self, location='Unknown', capacity=0, plates=None, sensors=None, displays=None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates
+        self.sensors = sensors or []
+        self.displays = displays
+
+    def __str__(self):
+        print(f"Car park at {self.location}, with {self.capacity} bays.")
+
 
